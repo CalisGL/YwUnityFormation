cec94c3f865ad7b727ea96b1fc3c06287b99eba1
Liska <calisgerentonlafaye@gmail.com>
2024-04-14 19:27:18 +0200

    Update move.cs

--- Assets/subway/scripts/move.cs
+++ Assets/subway/scripts/move.cs
@@ -17,11 +17,14 @@ public class move : MonoBehaviour
     public GameObject vie3;
     public GameObject onigiri;
     public bool onigiriTook;
+    private Vector3 fp;   //First touch position
+    private Vector3 lp;   //Last touch position
+    private float dragDistance;  //minimum distance for a swipe to be registered
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        dragDistance = Screen.height * 15 / 100;
     }
 
     // Update is called once per frame
@@ -29,29 +32,11 @@ public class move : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            if(voie == 2)
-            {
-                voie = 1;
-                transform.position = new Vector3(voie1, transform.position.y, transform.position.z);
-            }
-            else if(voie == 3)
-            {
-                voie = 2;
-                transform.position = new Vector3(voie2, transform.position.y, transform.position.z);
-            }
+            moveLeft();
         }
         else if(Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if(voie == 2)
-            {
-                voie = 3;
-                transform.position = new Vector3(voie3, transform.position.y, transform.position.z);
-            }
-            else if(voie == 1)
-            {
-                voie = 2;
-                transform.position = new Vector3(voie2, transform.position.y, transform.position.z);
-            }
+            moveRight();
         }
 
         if(touchAnOpponent())
@@ -109,8 +94,87 @@ public class move : MonoBehaviour
             gameover();
         }
 
+
+        if (Input.touchCount == 1) // user is touching the screen with a single touch
+        {
+            Touch touch = Input.GetTouch(0); // get the touch
+            if (touch.phase == TouchPhase.Began) //check for the first touch
+            {
+                fp = touch.position;
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved) // update the last position based on where they moved
+            {
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended) //check if the finger is removed from the screen
+            {
+                lp = touch.position;  //last touch position. Ommitted if you use list
+ 
+                //Check if drag distance is greater than 20% of the screen height
+                if (Mathf.Abs(lp.x - fp.x) > dragDistance || Mathf.Abs(lp.y - fp.y) > dragDistance)
+                {//It's a drag
+                 //check if the drag is vertical or horizontal
+                    if (Mathf.Abs(lp.x - fp.x) > Mathf.Abs(lp.y - fp.y))
+                    {   //If the horizontal movement is greater than the vertical movement...
+                        if ((lp.x > fp.x))  //If the movement was to the right)
+                        {   //Right swipe
+                            Debug.Log("Right Swipe");
+                            moveRight();
+                        }
+                        else
+                        {   //Left swipe
+                            Debug.Log("Left Swipe");
+                            moveLeft();
+                        }
+                    }
+                    else
+                    {   //the vertical movement is greater than the horizontal movement
+                        if (lp.y > fp.y)  //If the movement was up
+                        {   //Up swipe
+                            Debug.Log("Up Swipe");
+                        }
+                        else
+                        {   //Down swipe
+                            Debug.Log("Down Swipe");
+                        }
+                    }
+                }
+                else
+                {   //It's a tap as the drag distance is less than 20% of the screen height
+                    Debug.Log("Tap");
+                }
+            }
+        }
     }
 
+    void moveRight()
+    {
+        if(voie == 2)
+        {
+            voie = 3;
+            transform.position = new Vector3(voie3, transform.position.y, transform.position.z);
+        }
+        else if(voie == 1)
+        {
+            voie = 2;
+            transform.position = new Vector3(voie2, transform.position.y, transform.position.z);
+        }
+    }
+
+    void moveLeft()
+    {
+        if(voie == 2)
+        {
+            voie = 1;
+            transform.position = new Vector3(voie1, transform.position.y, transform.position.z);
+        }
+        else if(voie == 3)
+        {
+            voie = 2;
+            transform.position = new Vector3(voie2, transform.position.y, transform.position.z);
+        }
+    }
     bool touchAnOpponent()
     {
         RaycastHit hit;
